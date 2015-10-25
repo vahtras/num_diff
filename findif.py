c47a41f92d4f205746c34254e2ee366b67da5bac
Olav Vahtras <vahtras@kth.se>
2015-10-26 00:29:56 +0100

    split up

--- findif.py
+++ findif.py
@@ -4,28 +4,10 @@ def grad(f, delta=1e-5):
     def grad_f(*args, **kwargs):
         if len(args) == 1:
             x, = args
-            if type(x) in (float, int):
-                gradf_x = (
-                    f(x+delta/2) - f(x-delta/2)
-                    )/delta
-                return gradf_x
-            else:
-                if type(x) == numpy.ndarray:
-                    grad_val = numpy.zeros(x.shape)
-                    it = numpy.nditer(x, op_flags=['readwrite'], flags=['multi_index'])
-                    print dir(it)
-                    for xi in it:
-                        i = it.multi_index
-                        xi += delta/2
-                        fp = f(x)
-                        xi -= delta
-                        fm = f(x)
-                        xi += delta/2
-                        grad_val[i] = (fp - fm)/delta
-
-                    return grad_val
-                        
-
+            gradf_x = (
+                f(x+delta/2) - f(x-delta/2)
+                )/delta
+            return gradf_x
         elif len(args) == 2:
             x, y = args
             if type(x) in [float, int] and type(y) in [float, int]:
@@ -33,6 +15,24 @@ def grad(f, delta=1e-5):
                 gradf_y = (f(x, y + delta/2) - f(x, y - delta/2))/delta
                 return gradf_x, gradf_y
         
+    return grad_f
+
+def ndgrad(f, delta=1e-5):
+    def grad_f(*args, **kwargs):
+        x, = args
+        grad_val = numpy.zeros(x.shape)
+        it = numpy.nditer(x, op_flags=['readwrite'], flags=['multi_index'])
+        print dir(it)
+        for xi in it:
+            i = it.multi_index
+            xi += delta/2
+            fp = f(x)
+            xi -= delta
+            fm = f(x)
+            xi += delta/2
+            grad_val[i] = (fp - fm)/delta
+
+        return grad_val
     return grad_f
 
 
--- test_findif.py
+++ test_findif.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy
 
-from findif import grad
+from findif import grad, ndgrad
 
 class NewTest(unittest.TestCase):
 
@@ -26,13 +26,13 @@ class NewTest(unittest.TestCase):
         def f(x_arr):
             return numpy.dot(x_arr, x_arr)
         x = numpy.array((3., 4.))
-        numpy.testing.assert_allclose(grad(f)(x), (6, 8))
+        numpy.testing.assert_allclose(ndgrad(f)(x), (6, 8))
 
     def test_2_matrix(self):
         def f(x):
             return x[0, 0]**2 + x[1, 1]**2 - x[0, 1]*x[1, 0]
         x = numpy.array([[1., 2.], [3., 4.]])
-        numpy.testing.assert_allclose(grad(f)(x), [[2, -3], [-2, 8]])
+        numpy.testing.assert_allclose(ndgrad(f)(x), [[2, -3], [-2, 8]])
 
             
         
